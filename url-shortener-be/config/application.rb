6ef503d45d30f182d10f8106b792181521ef41c5
Billy Reisinger <billy.reisinger@gmail.com>
2021-11-04 10:01:36 -0500

    Api-only app; cleanup a few unused features

--- url-shortener-be/config/application.rb
+++ url-shortener-be/config/application.rb
@@ -1,6 +1,19 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,11 +21,12 @@ Bundler.require(*Rails.groups)
 
 module UrlShortenerBe
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.api_only = true
     config.load_defaults 6.1
     config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-    config.cache_store = :redis_store, ENV['CACHE_URL'],
-                     { namespace: 'url-shortener::cache' }
+    config.cache_store = :redis_cache_store, {
+      url: ENV['CACHE_URL'],
+      namespace: 'url-shortener::cache' }
     config.active_job.queue_adapter = :sidekiq 
   end
 end
